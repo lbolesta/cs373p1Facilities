2e87e0839976dfb71ff0bc08097084c70a4d228a
lbolesta <lbolesta@luc.edu>
2017-02-06 19:12:50 -0600

    first commit from eclipse

---
+++ src/facility/Facility.java
@@ -0,0 +1,11 @@
+package facility;
+
+public interface Facility {
+	public void listFacilities (); //choose params and returns 
+	public void getFacilityInformation(); //choose params and returns 
+	public void requestAvailableCapacity(); //choose params and returns 
+	public void addNewFacility(); //choose params and returns 
+	public void addFacilityDetail(); 
+	public void removeFacility();  //choose params and returns 
+
+}
